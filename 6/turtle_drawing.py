0ffaf50ae75fb4650ed9948b5cc1051da77445bd
Zickam <Zickam@mail.ru>
2022-11-22 20:42:32 +0500

    2

--- 6/turtle_drawing.py
+++ 6/turtle_drawing.py
@@ -22,50 +22,45 @@ def drawCoordinateSys():
     tom.pendown()
     tom.dot(DOT_SIZE * 2, "red")
 
-    tom.left(90)
-    tom.pendown()
-    tom.forward(SYS_SIZE * PIXELS_PER_1)
-    tom.penup()
-
-    tom.setposition((0, 0))
-    tom.right(90)
-    tom.pendown()
-    tom.forward(SYS_SIZE * PIXELS_PER_1)
-
-    tom.left(90)
-
-    tom.pendown()
-
-    for i in range(SYS_SIZE):
-        tom.penup()
-        tom.setposition((i * PIXELS_PER_1, 0))
+    degrees = [0, 90, 180, 270]
+    for degree in degrees:
+        tom.setposition((0, 0))
+        tom.setheading(degree)
         tom.pendown()
-
-        for j in range(SYS_SIZE):
-
-            tom.pendown()
-            tom.dot(DOT_SIZE, dot_color)
-            tom.penup()
-            tom.forward(PIXELS_PER_1)
-
-    tom.right(180)
-    for i in range(SYS_SIZE):
+        tom.forward(SYS_SIZE * PIXELS_PER_1)
         tom.penup()
-        tom.setposition((i * PIXELS_PER_1, 0))
-        tom.pendown()
 
-        for j in range(SYS_SIZE):
-            tom.pendown()
-            tom.dot(DOT_SIZE, dot_color)
-            tom.penup()
-            tom.forward(PIXELS_PER_1)
+    tom.setheading(90)
 
+    tom.pendown()
+
+    for degree in degrees:
+        tom.setheading(degree)
+
+        for x in range(-SYS_SIZE, SYS_SIZE):
+            for y in range(-SYS_SIZE, SYS_SIZE):
+                tom.penup()
+                tom.setposition(x * PIXELS_PER_1, y * PIXELS_PER_1)
+                tom.dot(DOT_SIZE, dot_color)
+
+
+    # tom.right(180)
+    # for i in range(SYS_SIZE):
+    #     tom.penup()
+    #     tom.setposition((i * PIXELS_PER_1, 0))
+    #     tom.pendown()
+    #
+    #     for j in range(SYS_SIZE):
+    #         tom.pendown()
+    #         tom.dot(DOT_SIZE, dot_color)
+    #         tom.penup()
+    #         tom.forward(PIXELS_PER_1)
 
         # print(i * PIXELS_PER_1)
 
 def drawByCommands():
-    commands = {"right": 120, "forward": 10 * PIXELS_PER_1, "right": 120}
-    repeat = 10
+    commands = [("right", 60), ("forward", 2 * PIXELS_PER_1), ("right", 60), ("forward", 2 * PIXELS_PER_1), ("right", 270)]
+    repeat = 14
 
     tom.penup()
     tom.setposition((0, 0))
@@ -73,7 +68,7 @@ def drawByCommands():
     tom.pendown()
 
     for _ in range(repeat):
-        for command, value in commands.items():
+        for command, value in commands:
             if command == "right":
                 tom.right(value)
             elif command == "left":
